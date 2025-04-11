5488071ba53a80daed9a59abdf4a4a7fcb70ef49
gappc <gapp.christian@gmail.com>
2025-04-11 16:32:08 +0200

    Add gap to layout in SensorMeasurements for improved spacing

--- src/components/sensorMeasurements/SensorMeasurements.vue
+++ src/components/sensorMeasurements/SensorMeasurements.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center gap-3">
     <div class="flex">
       <ButtonCustom
         class="rounded-r-none"
